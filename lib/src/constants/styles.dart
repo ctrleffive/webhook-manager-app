26ad6f8f3110e6ead6734b8154c5dc604a16af1b
Chandu J S <app.coder@outlook.in>
2020-04-13 17:51:36 +0530

    font, clor, styles

--- lib/src/constants/styles.dart
+++ lib/src/constants/styles.dart
@@ -3,10 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class StylesConstant {
-  static const Color primaryColor = Color(0xFF124893);
-  static const Color accentColor = Color(0xFF0076CA);
-
-  static const Color textColor = Colors.black87;
+  static const Color primaryColor = Color(0xFF7B71E9);
+  static const Color accentColor = Colors.black;
 
   static const Color googleColor = Color(0xFF4285F4);
   static const Color facebookColor = Color(0xFF3A549F);
@@ -14,14 +12,11 @@ class StylesConstant {
   static const double paddingSide = 20;
 
   static final ThemeData appTheme = ThemeData(
-    textTheme: GoogleFonts.ubuntuTextTheme(),
+    textTheme: GoogleFonts.robotoMonoTextTheme(),
     appBarTheme: AppBarTheme(
       elevation: 0,
       color: Colors.transparent,
       brightness: Brightness.light,
-      iconTheme: IconThemeData(
-        color: StylesConstant.textColor,
-      ),
     ),
     brightness: Brightness.light,
     primaryColor: StylesConstant.primaryColor,
