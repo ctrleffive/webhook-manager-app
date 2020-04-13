e3c3ea42c3c532a4d04bd8a8421d6d45f5f79c1e
Chandu J S <app.coder@outlook.in>
2020-04-13 19:07:24 +0530

    root page ui

--- lib/src/views/pages/root.dart
+++ lib/src/views/pages/root.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:webhook_manager/src/views/layouts/page_wrap.dart';
+
 class RootPage extends StatelessWidget {
   RootPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return PageWrap(
+      children: <Widget>[
+        SizedBox(),
+        Center(
+          child: CircularProgressIndicator(),
+        ),
+        SizedBox(),
+      ],
     );
   }
 }
