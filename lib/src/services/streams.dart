c05c6392e328ec58aeb2d42e582ce94a323092aa
Chandu J S <app.coder@outlook.in>
2020-04-14 12:26:21 +0530

    loader stream & ui added,

--- lib/src/services/streams.dart
+++ lib/src/services/streams.dart
@@ -2,9 +2,11 @@ import 'package:rxdart/subjects.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class StreamsService {
+  static final BehaviorSubject<bool> loaderState = BehaviorSubject<bool>.seeded(true);
   static final BehaviorSubject<FirebaseUser> sessionUser = BehaviorSubject<FirebaseUser>();
 
   static void dispose() {
+    loaderState?.close();
     sessionUser?.close();
   }
 }
--- lib/src/views/layouts/page_wrap.dart
+++ lib/src/views/layouts/page_wrap.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import 'package:webhook_manager/src/services/streams.dart';
+
 import 'package:webhook_manager/src/views/components/app_title.dart';
 
 class PageWrap extends StatelessWidget {
@@ -18,23 +23,59 @@ class PageWrap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: this.isCentered ? MainAxisAlignment.spaceBetween : MainAxisAlignment.start,
-            crossAxisAlignment: this.isCentered ? CrossAxisAlignment.center : CrossAxisAlignment.start,
-            children: [
-              if (this.title != null) 
-              AppTitle(
-                icon: this.icon,
-                label: this.title,
-                isCentered: this.isCentered,
+      body: Stack(
+        children: <Widget>[
+          SafeArea(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: this.isCentered
+                    ? MainAxisAlignment.spaceBetween
+                    : MainAxisAlignment.start,
+                crossAxisAlignment: this.isCentered
+                    ? CrossAxisAlignment.center
+                    : CrossAxisAlignment.start,
+                children: [
+                  if (this.title != null)
+                    AppTitle(
+                      icon: this.icon,
+                      label: this.title,
+                      isCentered: this.isCentered,
+                    ),
+                  ...this.children,
+                ],
               ),
-              ...this.children,
-            ],
+            ),
           ),
-        ),
+          StreamBuilder<bool>(
+            stream: StreamsService.loaderState,
+            initialData: StreamsService.loaderState.value,
+            builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+              return IgnorePointer(
+                ignoring: !snapshot.data,
+                child: AnimatedOpacity(
+                  opacity: snapshot.data ? 1 : 0,
+                  duration: Duration(milliseconds: 200),
+                  curve: Curves.easeInOut,
+                  child: ClipRect(
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                        sigmaX: 5,
+                        sigmaY: 5,
+                      ),
+                      child: Container(
+                        color: Colors.white10,
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }
+          ),
+        ],
       ),
     );
   }
